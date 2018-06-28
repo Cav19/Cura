b63c6968bd7e2510f2a3913d79caa550171930d1
cav19 <connor.valenti19@gmail.com>
2018-06-28 01:13:23 -0500

    trailing slash.

--- Src/docfinder/urls.py
+++ Src/docfinder/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('submit/', views.submit, name='submit'),
     path('results/', views.results, name='results'),
     path('import/', views.import_data, name='import'),
-    path('all_doctors', views.all_doctors, name='all_doctors')
+    path('all_doctors/', views.all_doctors, name='all_doctors')
 ]
